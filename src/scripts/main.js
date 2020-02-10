d5cf06c850ee95cec4606e6525d7eb92fef6dd9b
Senya Volkov <formobileprima@gmail.com>
2020-02-10 18:46:26 +0200

    fixed remarks2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,23 +5,25 @@ const dotList = [...document.querySelectorAll('.carousel__dot')];
 const carouselFirstItem = document.querySelector('.carousel__item');
 const carouselItems = [...document.querySelectorAll('.carousel__item')];
 
-const marginBetweenSlisers
+const marginBetweenSliders
   = parseFloat(getComputedStyle(carouselFirstItem).marginRight);
 
 let marginValue = 0;
-let check = null;
+let checkMarginValue = 0;
 
 function slidingPrev(eventPrev) {
   if (eventPrev.target.className.includes('carousel__btn_prev')) {
-    check = marginValue + parseFloat(getComputedStyle(carouselFirstItem).width)
-      + marginBetweenSlisers;
+    checkMarginValue
+      = marginValue
+      + parseFloat(getComputedStyle(carouselFirstItem).width)
+      + marginBetweenSliders;
 
-    if (check > 0) { // граница слайдера
+    if (checkMarginValue > 0) { // граница слайдера
       return false;
     }
 
     marginValue += parseFloat(getComputedStyle(carouselFirstItem).width)
-    + marginBetweenSlisers;
+    + marginBetweenSliders;
     // Асинхронный запуск - защита от множетсва кликов при transition
     marginSet();
   }
@@ -29,17 +31,18 @@ function slidingPrev(eventPrev) {
 
 function slidingNext(eventNext) {
   if (eventNext.target.className.includes('carousel__btn_next')) {
-    check = marginValue - parseFloat(getComputedStyle(carouselFirstItem).width)
-      + marginBetweenSlisers;
+    checkMarginValue = marginValue
+      - parseFloat(getComputedStyle(carouselFirstItem).width)
+      + marginBetweenSliders;
 
     if ( // граница слайдера
-      Math.abs(check) > (carouselItems.length
+      Math.abs(checkMarginValue) > (carouselItems.length
       * parseFloat(getComputedStyle(carouselFirstItem).width))) {
       return false;
     }
 
     marginValue -= parseFloat(getComputedStyle(carouselFirstItem).width)
-      + marginBetweenSlisers;
+      + marginBetweenSliders;
     // Асинхронный запуск - защита от множетсва кликов при transition
     marginSet();
   }
@@ -47,16 +50,19 @@ function slidingNext(eventNext) {
 
 function activeDot(margin) {
   const activeNumber
-    = Math.abs(Math.round(
-      margin / parseFloat(getComputedStyle(carouselFirstItem).width)
-    ));
+    = Math.abs(
+      Math.round(
+        margin / parseFloat(getComputedStyle(carouselFirstItem).width)
+      )
+    );
 
   dotList.forEach(dot => {
-    if (dot.className.includes('carousel__dot_active')) {
-      dot.classList.remove('carousel__dot_active');
+    if (dot.className.includes('carousel__dot--active')) {
+      dot.classList.remove('carousel__dot--active');
     }
   });
-  dotList[activeNumber].classList.add('carousel__dot_active');
+
+  dotList[activeNumber].classList.add('carousel__dot--active');
 }
 
 function marginSet() {
